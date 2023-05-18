612e7e211cd3d1f7fad33b7a1fbf09125f7420b8
Kauã Colins <kauamoreira3313@gmail.com>
2023-05-18 19:08:21 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump')
+
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 500);
+}
+
+const loop = setInterval(() => {
+    const pipeposition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log('loop')
+
+    if (pipeposition <= 120 && pipeposition > 0 && marioPosition < 80){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipeposition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './imgs/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+    }
+}, 10)
+
+document.addEventListener('keydown', jump)
